29d580043fbb76092cba6a8cd91c3cd9a918da7e
Diego de Lima Fogaça <diego.fogaca@ituran.com.br>
2024-09-18 18:03:09 -0300

    Atualização Icones Sol e Lua

--- Template/script/script.js
+++ Template/script/script.js
@@ -15,7 +15,7 @@ for (let i = 1; i <= numberOfCards; i++) {
     cardContainer.appendChild(card);
 }
 
-const themeToggle = document.getElementById('themeToggle');
+const themeToggle = document.querySelector('.theme-toggle');
 
 // Função para definir o cookie
 function setCookie(name, value, days) {
@@ -47,9 +47,29 @@ window.onload = () => {
 };
 
 // Alternar tema e salvar em cookie
+const sunIcon = document.querySelector('.material-symbols-outlined.day');
+const moonIcon = document.querySelector('.material-symbols-outlined.night');
+
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
     themeToggle.classList.toggle('active');
     const theme = document.body.classList.contains('dark-theme') ? "dark" : "light";
     setCookie("theme", theme, 7); // Salva o tema por 7 dias
+
+    // Alterna os ícones
+    if (theme === "dark") {
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'block';
+    } else {
+        sunIcon.style.display = 'block';
+        moonIcon.style.display = 'none';
+    }
 });
+
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
